ff3605bbdf8b2fb3bca12d2d5defa74778954718
Sayantan <sayantann2002@gmail.com>
2023-09-23 02:00:11 +0530

    Feat: Joi validation added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { mongoose } from "./mongoose";
 export { Tokens } from "./generateTokens";
 export { verify } from './verifyRefreshToken';
+export {logInBodyValidation,signUpBodyValidation,refreshTokenBodyValidation } from './validationSchema'
